172e737708a293655dcb44f796e1e6d18a782b59
psykid <psykid@users.noreply.github.com>
2014-05-01 18:56:23 +0530

    Update final_maclauren.py

--- final_maclauren.py
+++ final_maclauren.py
@@ -2,64 +2,51 @@ from sympy import *
 from math import *
 import math
 
-fun="1/x"
-h=0.1
-a=1
-b=2
-n=2
-num=int((b-a)/h)
+x=Symbol('x')
 
-def f(x):
+def f(x,fun):
     return eval(fun)
 
-
-def g(x,p):
-    global fun
-    der= fun
-    for q in range(0,p):
-        t=diff(der)
-        der=t
-    print ((der)).evalf(2)," dafa"
+def g(x,p,fun):
+    y=x
+    x=Symbol('x')
+    der= diff(fun,x,p)
+    x=y
     return 1.0*(eval(str(der)))
 
-#print bernoulli(2)
-coeff=[]
-for i in range(1,n+1):
-    dummy=bernoulli(2*i)/math.factorial(2*i)
-    print math.factorial(2*i)
-    coeff+=[dummy]
+#fun="1/x"
+def maclauren__(fun,a,b):
+    h=0.1
+    #a=1
+    #b=2
+    n=2
+    num=int((b-a)/h)
 
-print coeff
-y=[]
-k=int((b-a)/h)
-for i in range (0,k+1):
-	y+=[a+i*h]
-#print x
-sum1=0
+    coeff=[]
+    for i in range(1,n+1):
+        dummy=bernoulli(2*i)/math.factorial(2*i)
+        coeff+=[dummy]
 
-for j in range(1,k):
-	sum1+=f(y[j])*h	
+    y=[]
+    k=int((b-a)/h)
+    for i in range (0,k+1):
+            y+=[a+i*h]
+    sum1=0
 
-sum2=(f(y[0])+f(y[k]))*h/2
+    for j in range(1,k):
+            sum1+=f(y[j],fun)*h	
 
-summ=sum1+sum2
-sum2=0
-print 'from trapezoid rule :/', summ
-#print g(b,b)
-for i in range(0,len(coeff)):
-    #print coeff[i]
-    sum1=float(coeff[i]*(g(a,2*i+1)-g(b,2*i+1))*h**(2*i+2))
-    print (g(a,2*i+1)-g(b,2*i+1)),"from diff,i ", i
-    print h**(2*i+1)," from h"
-    print sum1," from sum1 i ",i
-    sum2+=sum1
+    sum2=(f(y[0],fun)+f(y[k],fun))*h/2
 
-#print g(3,1)
-#print float(g(10,1))
-print sum2
-x=Symbol('x')
-z=diff(diff("x**2"))
-def f1(x):
-    return float(eval(str(z)))
-print f1(3),f1(2)," from f1,3,2"
-print float(1/(2**4))
+    summ=sum1+sum2
+    sum2=0
+    #print 'from trapezoid rule :/', summ
+    #print g(b,b)
+    for i in range(0,len(coeff)):
+        sum1=float(coeff[i]*(g(a,2*i+1,fun)-g(b,2*i+1,fun))*h**(2*i+2))
+        sum2+=sum1
+
+    return sum2+summ
+
+hal=maclauren__("1/x",1,2)
+print hal
